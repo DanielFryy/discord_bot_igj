b8ce9029e0ee460225a30c4e98817a0374f0750b
danielfryy <daniel.freire@trade.ec>
2020-10-08 23:53:33 -0500

    Add new cron and control for same urls

--- src/bot.js
+++ src/bot.js
@@ -39,17 +39,32 @@ cron.schedule("30 22 * * *", () => {
   channel.send(`Ya anda a dormir ${danielfryy}`);
 });
 
+
+cron.schedule("30 23 * * *", () => {
+  const channel = client.channels.cache.get(
+    process.env.DISCORD_GENERAL_TEXT_CHANNEL_ID
+  );
+  const danielfryy = client.users.cache.get(process.env.DISCORD_MY_USER_ID);
+  channel.send(`Que ya vayas a dormir pto ${danielfryy}`);
+});
+
+let lastUrl = "";
+
 cron.schedule("30 * * * *", () => {
   const channel = client.channels.cache.get(
     process.env.DISCORD_MEMES_TEXT_CHANNEL_ID
   );
-  fetch("https://9gag.com/v1/group-posts/group/wtf/type/latest")
+  fetch("https://9gag.com/v1/group-posts/group/wtf/type/hot")
     .then(res => res.json())
     .then(res => {
       const { data } = res;
       const { posts } = data;
       const [firstPost] = posts;
       const { url } = firstPost || {};
+      if (url === lastUrl) {
+        channel.send("Está el mismo meme de antes, ya nada =(.");
+        return;
+      }
       channel.send(url ? url : "Algo falló obteniendo la url del post =(.");
     })
     .catch(err => console.log("xxxError", err));
