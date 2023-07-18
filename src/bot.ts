c1439f8513b9a9003adc8421fb418214ffcbbb6b
DanielFryy <dgz1606@gmail.com>
2023-07-17 22:17:22 -0500

    remove unnecessary function wrapper for discord client

--- src/bot.ts
+++ src/bot.ts
@@ -6,57 +6,53 @@ import { config } from "dotenv";
 import readyListener from "./listeners/ready";
 import voiceStateUpdateListener from "./listeners/voiceStateUpdate";
 
-const bot = async () => {
-  config();
+config();
 
-  const client = new Client({
-    intents: [GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.Guilds]
-  });
+const client = new Client({
+  intents: [GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.Guilds]
+});
 
-  readyListener(client);
-  voiceStateUpdateListener(client);
+readyListener(client);
+voiceStateUpdateListener(client);
 
-  // Schedule tasks to be run on the server.
-  // cron.schedule("30 22 * * *", () => {
-  //   const channel = client.cache.get(
-  //     process.env.DISCORD_GENERAL_TEXT_CHANNEL_ID!
-  //   );
-  //   const danielfryy = client.users.cache.get(process.env.DISCORD_MY_USER_ID!);
-  //   channel?.send(`Ya anda a dormir ${danielfryy}`);
-  // });
+// Schedule tasks to be run on the server.
+// cron.schedule("30 22 * * *", () => {
+//   const channel = client.cache.get(
+//     process.env.DISCORD_GENERAL_TEXT_CHANNEL_ID!
+//   );
+//   const danielfryy = client.users.cache.get(process.env.DISCORD_MY_USER_ID!);
+//   channel?.send(`Ya anda a dormir ${danielfryy}`);
+// });
 
-  // cron.schedule("30 23 * * *", () => {
-  //   const channel = client.channels.cache.get(
-  //     process.env.DISCORD_GENERAL_TEXT_CHANNEL_ID
-  //   );
-  //   const danielfryy = client.users.cache.get(process.env.DISCORD_MY_USER_ID);
-  //   channel.send(`Que ya vayas a dormir pto ${danielfryy}`);
-  // });
+// cron.schedule("30 23 * * *", () => {
+//   const channel = client.channels.cache.get(
+//     process.env.DISCORD_GENERAL_TEXT_CHANNEL_ID
+//   );
+//   const danielfryy = client.users.cache.get(process.env.DISCORD_MY_USER_ID);
+//   channel.send(`Que ya vayas a dormir pto ${danielfryy}`);
+// });
 
-  // let lastUrl = "";
+// let lastUrl = "";
 
-  // cron.schedule("30 * * * *", () => {
-  //   const channel = client.channels.cache.get(
-  //     process.env.DISCORD_MEMES_TEXT_CHANNEL_ID
-  //   );
-  //   fetch("https://9gag.com/v1/group-posts/group/funny/type/hot")
-  //     .then(res => res.json())
-  //     .then(res => {
-  //       const { data } = res;
-  //       const { posts } = data;
-  //       const [firstPost] = posts;
-  //       const { url } = firstPost || {};
-  //       if (url === lastUrl) {
-  //         channel.send("Está el mismo meme de antes, ya nada =(.");
-  //         return;
-  //       }
-  //       lastUrl = url;
-  //       channel.send(url ? url : "Algo falló obteniendo la url del post =(.");
-  //     })
-  //     .catch(err => console.log("xxxError", err));
-  // });
+// cron.schedule("30 * * * *", () => {
+//   const channel = client.channels.cache.get(
+//     process.env.DISCORD_MEMES_TEXT_CHANNEL_ID
+//   );
+//   fetch("https://9gag.com/v1/group-posts/group/funny/type/hot")
+//     .then(res => res.json())
+//     .then(res => {
+//       const { data } = res;
+//       const { posts } = data;
+//       const [firstPost] = posts;
+//       const { url } = firstPost || {};
+//       if (url === lastUrl) {
+//         channel.send("Está el mismo meme de antes, ya nada =(.");
+//         return;
+//       }
+//       lastUrl = url;
+//       channel.send(url ? url : "Algo falló obteniendo la url del post =(.");
+//     })
+//     .catch(err => console.log("xxxError", err));
+// });
 
-  await client.login(process.env.DISCORD_IGJ_TOKEN);
-};
-
-bot();
+client.login(process.env.DISCORD_IGJ_TOKEN);
